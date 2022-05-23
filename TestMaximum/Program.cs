58ca0fe70eed50f02db889929e403680c573eafd
Rupasappati <rupasappati@gmail.com>
2022-05-23 13:24:16 +0530

    Added: Given 3 Floats find the maximum

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -1,26 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TestMaximum
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Welcome Message
+            Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
+
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+
+            //Maximum Number At First Place
+            double outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1, 2.82, 7.33);
+            Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
             
-                //Welcome Message
-                Console.WriteLine("Welcome to Maximum Number");
+            //Maximum Number At Second Place
+            double outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1, 28.2, 12.8);
+            Console.WriteLine("Maximum Number of Integer at Second Place : " + outputFloat2);
 
-                //Calling Class to call a Function
-                MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            //Maximum Number At Third Place
+            double outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9, 68.47, 77.3);
+            Console.WriteLine("Maximum Number of Integer at Third Place : " + outputFloat3);
 
-
-                //Maximum Number At Third Place
-                int outputInteger = maximumNumberCheck.MaximumIngerNumber(141, 282, 733);
-
-
-               //Display Output
-               Console.WriteLine("Maximum Number of Integer : " + outputInteger);
-  
+            Console.ReadLine();
         }
     }
 }
