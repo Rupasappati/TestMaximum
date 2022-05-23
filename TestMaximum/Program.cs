46549ee9ab5a2ab4e3c90a65f366def76c854c1e
Rupasappati <rupasappati@gmail.com>
2022-05-23 12:37:31 +0530

    Added: Finding the maximum Number

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Maximum Number");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // Welcome Message
+            Console.WriteLine("\n");
+            Console.WriteLine("------------Welcome to Find the Maximum Number Program----------");
+            Console.WriteLine("\n");
+
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11, 22, 33);
+
+            //Display Output
+            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+
+            Console.ReadLine();
+        }
+    }
+}
