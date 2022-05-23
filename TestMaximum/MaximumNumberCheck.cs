96e54efcf4fb96cfa2f28b4d01e7efe78db1999e
Rupasappati <rupasappati@gmail.com>
2022-05-23 14:05:02 +0530

    Added: Refactor all the 3 to One Generic Method and find the maximum

--- TestMaximum/MaximumNumberCheck.cs
+++ TestMaximum/MaximumNumberCheck.cs
@@ -63,5 +63,24 @@ namespace TestMaximum
             throw new Exception("First, Second and Third number are same : ");
         }
 
+        //Function To check Maximum Number Using Generics Method
+        public T MaximumNumber<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("First, Second and Third number are same : ");
+        }
+
+
     }
 }
